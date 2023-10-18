10f48027c43fd2713bbc2bb9c65d6c04743bba00
alexmelo47 <147992197+alexmelo47@users.noreply.github.com>
2023-10-18 21:27:10 +0400

    Add files via upload

---
+++ SSTUTestSystem/resources/views/test.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('page_title')Главная страница
+@endsection
+
+@section('content')
+    <main>
+        <div class="content-block">
+            <fieldset>
+                <legend><h3>&nbsp;матан 1&nbsp;</h3></legend>
+                <ul class="link-list">
+                    <li><a href="/test">Вопрос 1</a></li>
+                    <li><a href="#">Вопрос 2</a></li>
+                    <li><a href="#">Вопрос 3</a></li>
+                    <li><a href="#">Вопрос 4</a></li>
+                </ul>
+            </fieldset>
+        </div>
+    </main>
+@endsection
